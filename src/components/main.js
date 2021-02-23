b4e2d9ec5fddd8704add9d745ec9002dd079418b
Ruslan Nagiev <ultraduck100@gmail.com>
2021-02-24 00:45:12 +0300

    feat: add options

--- src/components/main.js
+++ src/components/main.js
@@ -1,13 +1,34 @@
 import React from "react";
 
 import ExpressionBox from "./expression-box.js";
-import Counter from "./counter.js";
+import Timer from "./timer.js";
+import Variants from "./variants/variants.js";
+import StartGame from "./start-game.js";
+import Settings from "./settings/settings.js";
 
 const Main = () => {
+  const gameField = (
+    <>
+      <ExpressionBox />
+      <Timer />
+      <Variants />
+    </>
+  );
+
+  const startGame = (
+    <StartGame />
+  );
+
+  const settings = (
+    <Settings />
+  );
+
   return(
     <main className='main'>
-      <ExpressionBox />
-      <Counter />
+      {
+
+      settings
+      }
     </main>
   );
 };
