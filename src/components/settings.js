ac8dd2acc5ee10e7efe950ebbdacedf8fead54a5
Ruslan Nagiev <ultraduck100@gmail.com>
2021-02-24 00:18:38 +0300

    feat: add new components

---
+++ src/components/settings.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Settings = () => {
+  return(
+    <div className='settings-wrap'>
+      <div className='settings'>
+
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
---
+++ src/components/start-game.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const StartGame = () => {
+
+  return(
+    <div className='start-btn-wrap'>
+      <div className='start-btn'>
+        start game
+      </div>
+    </div>
+  );
+};
+
+export default StartGame;
