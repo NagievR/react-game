757ba9cf12fb1a4a8544a8465332f9d67dece796
Ruslan Nagiev <ultraduck100@gmail.com>
2021-02-25 01:12:55 +0300

    refactor: get more suitable name

---
+++ src/components/start-game-button.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const StartGameButton = () => {
+
+  return(
+    <div className='start-btn-wrap'>
+      <div className='start-btn scale-anim'>
+        start game
+      </div>
+    </div>
+  );
+};
+
+export default StartGameButton;
