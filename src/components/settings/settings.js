4b52fed2638cfdaad635d42b05172f3a9c5280c8
Ruslan Nagiev <ultraduck100@gmail.com>
2021-02-25 02:47:57 +0300

    feat: add new option

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 import ModalWrap from "../modal-wrap";
-import AudioSettingsBlock from "./audio-settings-block";
+import AudioSettingsBlock from "./audio-settings/audio-settings-block";
+import NumbersRangeBlock from "./numbers-range/numbers-range-block";
+import OperatorsChoose from "./operators-choose";
 import ThemeChoose from "./theme-choose";
-
 
 const Settings = () => {
   return(
     <ModalWrap>
       <AudioSettingsBlock />
       <ThemeChoose />
+      <NumbersRangeBlock />
+      <OperatorsChoose />
     </ModalWrap>
   );
 };
