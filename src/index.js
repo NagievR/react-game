2e9a022440f6839eb63976af3d0e3f08aec4ce98
Ruslan Nagiev <ultraduck100@gmail.com>
2021-02-26 02:30:13 +0300

    feat: add context providers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { HandlersProvider } from './logic/handlers';
+import { DefineActionProvider } from './logic/define-action';
+import { StoreProvider } from './logic/store';
+
+
+const appWrapped = (
+  <StoreProvider>
+    <HandlersProvider>
+      <DefineActionProvider>
+        <App />
+      </DefineActionProvider>
+    </HandlersProvider>
+  </StoreProvider>
+);
 
 ReactDOM.render(
-  <App />,
+  appWrapped,
   document.getElementById('root')
 );
