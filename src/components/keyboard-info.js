eb63cccd64fb5993ee1c350001ec6de4b051740f
Ruslan Nagiev <ultraduck100@gmail.com>
2021-02-26 02:33:29 +0300

    refactor: minor code refactor

--- src/components/keyboard-info.js
+++ src/components/keyboard-info.js
@@ -2,22 +2,22 @@ import React from "react";
 import ModalWrap from "./modal-wrap";
 
 const KeyboardInfo = () => {
-    return(
-      <ModalWrap>
-        <article className='keyboard-info'>
-          <h3>Keyboard info</h3>
-          <p>
-            Use keys from
-            &nbsp;<span className='key-descr'>1</span>&nbsp;
-            to 
-            &nbsp;<span className='key-descr'>6</span>&nbsp;
-            to choose a variant then press
-            &nbsp;<span className='key-descr'>Enter</span>&nbsp;
-            to confirm your answer.
-          </p>
-        </article>     
-      </ModalWrap>
-    );
-  };
+  return(
+    <ModalWrap>
+      <article className='keyboard-info'>
+        <h3>Keyboard info</h3>
+        <p>
+          Use keys from
+          &nbsp;<span className='key-descr'>1</span>&nbsp;
+          to 
+          &nbsp;<span className='key-descr'>6</span>&nbsp;
+          to choose a variant then press
+          &nbsp;<span className='key-descr'>Enter</span>&nbsp;
+          to confirm your answer.
+        </p>
+      </article>     
+    </ModalWrap>
+  );
+};
   
-  export default KeyboardInfo;
+export default KeyboardInfo;
