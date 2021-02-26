409b69fb785c7cf5279030c350f7b255b3e6b7f4
Ruslan Nagiev <ultraduck100@gmail.com>
2021-02-26 03:07:36 +0300

    refactor: change close modal btn

--- src/components/modal-wrap.js
+++ src/components/modal-wrap.js
@@ -4,6 +4,7 @@ const ModalWrap = ({ children }) => {
   return(
     <div className='modal-wrap'>
       <div className='modal-content'>
+        <div className='modal-close'></div>
         {children}
       </div>
     </div>
