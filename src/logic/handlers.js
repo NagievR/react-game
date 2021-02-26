e9e0c240a5aa914c6a64a0475b872bfa789f8aaf
Ruslan Nagiev <ultraduck100@gmail.com>
2021-02-26 20:12:57 +0300

    feat: add math expression generator

--- src/logic/handlers.js
+++ src/logic/handlers.js
@@ -11,7 +11,8 @@ export const HandlersProvider = ({ children }) => {
   
   const {
     sectionToShow,
-    setSectionToShow
+    setSectionToShow,
+    setMathExpression,
   } = useStore();
 
   const switchSection = toShow => {
@@ -34,9 +35,47 @@ export const HandlersProvider = ({ children }) => {
     localStorageManager.set('sectionToShow', switchedObj);
   };
 
+  const generateMathExpression = () => {
+    
+    // temporary values: 
+      const min = 1;
+      const max = 20;
+      const choseOperators = ['*', '+', '-', '/'];
+      const exprLength = 1;
+    // ================
+
+    const randomize = (from, to) => {
+      return Math.round(Math.random() * (to - from) + from);
+    };
+
+    let expr = '';
+    for (let i = 0; i < exprLength + 1; i++) {
+      expr += randomize(min, max);
+
+      for (let j = 0; j < 1; j++) {
+        if (i >= exprLength) {
+          break;
+        }
+        const randIdx = randomize(0, choseOperators.length - 1);
+        expr += choseOperators[randIdx];
+      }
+    }
+
+    const newExpr = {
+      expression: expr, // eslint-disable-next-line
+      result: Math.round(eval(expr))
+    };
+
+    console.log(newExpr.result);
+
+    setMathExpression(newExpr);
+    localStorageManager.set('mathExpression', newExpr);
+  };
+
   const context = {
     switchSection,
     closeSections,
+    generateMathExpression,
   };
 
   return (
