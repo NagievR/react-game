b39209362cb042fde33c75c223916868e9fe3713
Ruslan Nagiev <ultraduck100@gmail.com>
2021-02-27 14:44:44 +0300

    feat: add logic of creating a new math expression

--- src/logic/handlers.js
+++ src/logic/handlers.js
@@ -38,38 +38,86 @@ export const HandlersProvider = ({ children }) => {
   const generateMathExpression = () => {
     
     // temporary values: 
-      const min = 1;
-      const max = 20;
+      const min = -5;
+      const max = 30;
       const choseOperators = ['*', '+', '-', '/'];
-      const exprLength = 1;
+      const exprLength = 3;
     // ================
+
+
+    // ======== helpers =========
 
     const randomize = (from, to) => {
       return Math.round(Math.random() * (to - from) + from);
     };
 
-    let expr = '';
+    function shuffle(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+    }
+
+    // ====== /helpers ==========
+
+    let newExpression = '';
     for (let i = 0; i < exprLength + 1; i++) {
-      expr += randomize(min, max);
+      const randNumber = randomize(min, max);
+
+      if (randNumber === 0) {
+        i--;
+        continue;
+      } else if (randNumber < 0) {
+        newExpression += `(${randNumber})`
+      } else {
+        newExpression += randNumber;
+      }
 
       for (let j = 0; j < 1; j++) {
         if (i >= exprLength) {
           break;
         }
         const randIdx = randomize(0, choseOperators.length - 1);
-        expr += choseOperators[randIdx];
+        newExpression += ` ${choseOperators[randIdx]} `;
       }
     }
 
-    const newExpr = {
-      expression: expr, // eslint-disable-next-line
-      result: Math.round(eval(expr))
+    // eslint-disable-next-line
+    const newResult = Math.round(eval(newExpression));
+
+    const newVariants = [];
+    newVariants.push(newResult);
+
+    for (let i = 0; i < 5; i++) {
+      
+      let from = newResult - 10 * String(newResult).length;
+      let to = newResult + 10 * String(newResult).length;
+      
+      if (from > to) {
+        [to, from] = [from, to];
+      }
+
+      const randNumber = randomize(from, to);
+      if (newVariants.includes(randNumber)) {
+        i--;
+      } else {
+        newVariants.push(randNumber);
+      }
+    }
+
+    shuffle(newVariants);
+
+    const newMathExpression = {
+      expression: newExpression, 
+      result: newResult, 
+      correctAnswerIdx: newVariants.indexOf(newResult),
+      userAnswerIdx: null,
+      variants: newVariants,
     };
 
-    console.log(newExpr.result);
-
-    setMathExpression(newExpr);
-    localStorageManager.set('mathExpression', newExpr);
+    console.log(newMathExpression);
+    setMathExpression(newMathExpression);
+    localStorageManager.set('mathExpression', newMathExpression);
   };
 
   const context = {
