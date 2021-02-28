2f2862e231976f85b36d979f9233e739999bb32b
Ruslan Nagiev <ultraduck100@gmail.com>
2021-03-01 01:49:09 +0300

    feat: add min max numbers range setting

--- src/components/settings/numbers-range/number-range.js
+++ src/components/settings/numbers-range/number-range.js
@@ -1,12 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const NumberRange = ({ name }) => {
+const NumberRange = ({ name, id, handleChange, value }) => {
   return(
     <div className='multiply-setting-wrap'>
       <form className='number-range'>
         <span>{`${name}:`}</span>
-        <input type="number" />
+        <input 
+          id={id} 
+          value={value}
+          onChange={handleChange}
+          type="number"
+          maxLength="4"
+          max="999"
+          min="-999"
+        />
       </form>
     </div>
   );
@@ -14,6 +22,9 @@ const NumberRange = ({ name }) => {
 
 NumberRange.propTypes = {
   name: PropTypes.string,
+  id: PropTypes.string,
+  handleChange: PropTypes.func,
+  value: PropTypes.number
 };
 
 export default NumberRange;
--- src/components/settings/numbers-range/numbers-range-block.js
+++ src/components/settings/numbers-range/numbers-range-block.js
@@ -1,12 +1,33 @@
 import React from "react";
+import useDefineAction from "../../../logic/define-action";
+import useStore from "../../../logic/store";
 import NumberRange from "./number-range";
+import { numbersRange } from "../../../logic/consts.js";
 
 const NumbersRangeBlock = () => {
 
+  const { gameSettings } = useStore();
+  const { defineAction } = useDefineAction();
+
+  const handleChange = event => {
+    const targ = event.target;
+    defineAction(numbersRange, [targ.id, targ.value]);
+  };
+
   return(
     <div className='settings-block'> 
-      <NumberRange name={'Min number'} />
-      <NumberRange name={'Max number'} />
+      <NumberRange 
+        name={'Min number'}
+        id={'minNumber'} 
+        handleChange={handleChange}
+        value={gameSettings.minNumber}
+      />
+      <NumberRange 
+        name={'Max number'}
+        id={'maxNumber'} 
+        handleChange={handleChange}
+        value={gameSettings.maxNumber}
+      />
     </div>
   );
 };
